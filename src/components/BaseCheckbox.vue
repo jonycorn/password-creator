08c8247b1c94b32e32995cae275c3fef03ac8edf
jonycorn <jonas.vybiral.jv@gmail.com>
2021-12-19 21:11:58 +0100

    all bugs should be fixed, just need to finish styling

--- src/components/BaseCheckbox.vue
+++ src/components/BaseCheckbox.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="flex justify-between appearance-none p-1">
+  <div
+    class="
+      flex
+      justify-between
+      appearance-none
+      p-4
+      sm:p-2
+      shadow-[0_0_10px_2px_rgba(0,0,0,0.1)]
+      rounded-md
+      sm:shadow-none
+    "
+    @click="emitChange"
+  >
     <label
-      class="text-gray-400 transition-all"
-      :class="{ 'text-black': checkState }"
+      class="text-gray-400 transition-all duration-500 pointer-events-none"
+      :class="{ 'text-black dark:text-white': curState }"
       :for="checkType"
     >
       {{ declareName }}</label
     >
-    <input
-      @change="this.$emit('drip', $event.target)"
-      v-model="checkState"
-      :id="checkType"
-      type="checkbox"
-    />
+    <input v-model="curState" :id="checkType" type="checkbox" />
   </div>
 </template>
 
@@ -28,9 +35,19 @@ export default {
         numbers: "Numbers",
         symbols: "Symbols",
       },
+      curState: this.checkState,
     };
   },
-
+  methods: {
+    emitChange(e) {
+      this.curState = !this.curState;
+      this.$emit("drip", {
+        state: this.curState,
+        el: e,
+        elName: this.checkName,
+      });
+    },
+  },
   computed: {
     declareName() {
       const key = Object.keys(this.checkNames).find(
